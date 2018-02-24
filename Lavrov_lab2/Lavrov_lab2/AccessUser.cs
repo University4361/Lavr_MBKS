4fe8c76a3e01a91bdcedc45d354ecd29d764d4e8
Ilya Lavrov <ilyalavrov97@gmail.com>
2018-02-24 17:30:25 +0300

    lab_2 completed

---
+++ Lavrov_lab2/Lavrov_lab2/AccessUser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lavrov_lab2
+{
+    public class AccessUser
+    {
+        private const string _template = "abcdefghijklmnopqrstuvwxyz";
+        public Dictionary<char, byte> AccessDictionary;
+
+        public AccessUser(string accessString)
+        {
+            InitializeDictionary(accessString);
+        }
+
+        private void InitializeDictionary(string accessString)
+        {
+            AccessDictionary = new Dictionary<char, byte>();
+
+            for (int i = 0; i < _template.Count(); i++)
+            {
+                bool result = false;
+                byte accessPoint = 0;
+
+                if (i <= accessString.Length - 1)
+                    result = byte.TryParse(accessString[i].ToString(), out accessPoint);
+
+                AccessDictionary.Add(_template[i], accessPoint);
+
+            }
+        }
+    }
+}
---
+++ Lavrov_lab2/Lavrov_lab2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lavrov_lab2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _currentCode;
+        private AccessUser _currentAccessUser;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            FirstTB.Text = string.Empty;
+            SecondTB.Text = string.Empty;
+            ThridTB.Text = string.Empty;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FirstRB.IsChecked ?? false)
+                _currentCode = FirstTB.Text;
+            if (SecondRB.IsChecked ?? false)
+                _currentCode = SecondTB.Text;
+            if (ThridRB.IsChecked ?? false)
+                _currentCode = ThridTB.Text;
+
+            _currentAccessUser = new AccessUser(_currentCode);
+        }
+
+        private void InputTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(_currentCode) || _currentAccessUser == null)
+                return;
+
+            OutputTB.Text = string.Empty;
+
+            foreach (var ch in InputTB.Text)
+            {
+                if (_currentAccessUser.AccessDictionary.ContainsKey(ch) && _currentAccessUser.AccessDictionary[ch] == 1)
+                    OutputTB.Text += ch;
+            }
+        }
+    }
+}
